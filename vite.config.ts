3a4a90876c8820a2c4ace9b425970319c7ebceaa
sok <sokkeneter@outlook.com>
2026-06-24 15:43:26 +0200

    add base url

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base:"https://sokkeneter.github.io/SokkeneterPersonalWebsite/",
   plugins: [
     vue(),
     vueJsx(),
